519c97e50b00b5b07e5c60e83a63e424b3932055
nikunj <nikupatel199@gmail.com>
2018-11-25 14:57:36 -0600

    Recepi

---
+++ assets/javascript/recipe.js
@@ -0,0 +1,154 @@
+
+$(".secondpage").hide();
+var queryURLbase = "https://api.edamam.com/search?&app_id=192e6853&app_key=97cc74f29550dbca8f09e9ac463a150f&from=0&to=13&q=";
+var ingShowList = [];
+var digests = [];
+var calories = [];
+var weights = [];
+var coockTimes = [];
+var instructions = [];
+var imageShowList = [];
+var nameRecepi = [];
+var totalingredients = [];
+var ingWeightList = [];
+var nutritionFacts = [];
+var nutritionTotal = [];
+function doAjax(queryURL) {
+	fetch(queryURL)
+		.then((resp) => resp.json())
+		.then(function (data) {
+			console.log(data);
+			var flex = $('<div class="row">');
+			for (var i = 0; i < 12; i++) {
+				var ingList = $(`<ul class="ul">
+									<li>
+									<h4>Ingerediant</h4>
+									</li>
+									`
+					// <th scope="col">Weight(ounce)</th>
+
+				);
+				var weightList = $(`<ul class="ul">
+									<li>
+										<h4>Weight(ounce)</h4>
+									</li>
+									`
+				);
+				var nutritionfacts = $(`<ul class="ul">
+									<li>
+										<h4>Nutrition Facts</h4>
+									</li>
+									`
+				);
+				var nutritiontotal = $(`<ul class="ul">
+									<li>
+										<h4>Total</h4>
+									</li>
+									`
+				);
+				var digestList = $(`<table class="table">
+								<thead class="thead-dark">
+								<tr>
+									<th scope="col">Nutrition facts</th>
+									<th scope="col">Total</th>
+								</tr>
+								</thead>
+								<tbody>`
+				);
+
+				for (var j = 0; j < data.hits[i].recipe.ingredients.length; j++) {
+					var ingcon = data.hits[i].recipe.ingredients.length;
+					totalingredients.push(ingcon);
+					var ing = data.hits[i].recipe.ingredients[j].text;
+					var ingweight = data.hits[i].recipe.ingredients[j].weight;
+					ingList.append(`<li>${ing}</li>`);
+					weightList.append(`<li>${ingweight}</li>`);
+				}
+				for (var j = 0; j < data.hits[i].recipe.digest.length; j++) {
+					var dig = data.hits[i].recipe.digest[j].label;
+					var digTotal = data.hits[i].recipe.digest[j].total;
+					digestList.append(`<tr class="table-warning"><td class="table-warning">${dig}</td><td class="table-warning">${digTotal}</td></tr>`)
+					nutritionfacts.append(`<li>${dig}</li>`);
+					nutritiontotal.append(`<li>${digTotal}</li>`);
+				}
+				calories.push(data.hits[i].recipe.calories);
+				weights.push(data.hits[i].recipe.totalWeight);
+				coockTimes.push(data.hits[i].recipe.totalTime);
+				instructions.push(data.hits[i].recipe.url);
+				digests.push(digestList);
+				ingShowList.push(ingList);
+				ingWeightList.push(weightList);
+				nutritionFacts.push(nutritionfacts);
+				nutritionTotal.push(nutritiontotal);
+
+
+				var card = $(`<div data-content=${i} class="radius bordered shadow card addVids">`);
+				var img = $("<img class='image '>");
+				imgAPI = data.hits[i].recipe.image;
+				imageShowList.push(imgAPI);
+				img.attr("src", imgAPI);
+				card.append(img);
+
+				title = data.hits[i].recipe.label;
+				nameRecepi.push(title);
+				// var link = $(`<a href="#" style="text-decoration:none;" >`);
+				var sec = $("<div class='card-divider'>")
+				var span = $('<p style="margin-top:10px;">');
+				span.text(title);
+				sec.append(span);
+				// sec.append(link);
+				card.append(sec);
+				flex.append(card);
+
+
+			};
+			$('.recipeList').append(flex);
+		});
+};
+
+$(document).on('click', '.addVids', function () {
+	$(".secondpage").show();
+	var imgsrc = (imageShowList[parseInt($(this).attr('data-content'))]);
+	var im = $("<img class='thumbnail'>");
+	im.attr("src", imgsrc);
+	$(".secondpageimg").append(im);
+	$(".name").append(nameRecepi[parseInt($(this).attr('data-content'))])
+	$('.arti').append(ingShowList[parseInt($(this).attr('data-content'))]);
+	$('.wei').append(ingWeightList[parseInt($(this).attr('data-content'))]);
+	$('.nutritions').append(nutritionFacts[parseInt($(this).attr('data-content'))]);
+	$('.nutritionstot').append(nutritionTotal[parseInt($(this).attr('data-content'))]);
+	$('.article').empty();
+	$('.article').append(`<button type="button" class="btn btn-primary back">Back</button>`);
+	$('.article').append('<hr>');
+	$('.article').append('<h2>Ingredients</h2>');
+	// $('.article').append(ingShowList[parseInt($(this).attr('data-content'))]);
+	$('.article').append('<hr>');
+	$('.article').append('<h2>Nutritions</h2>');
+	// $('.article').append(digests[parseInt($(this).attr('data-content'))]);
+	$('.article').append('<hr>');
+	$('.article').append('<h2>Calories</h2>');
+	$('.article').append(`<h3>${calories[$(this).attr('data-content')]}</h3>`);
+	$('.article').append('<hr>');
+	$('.article').append('<h2>Weight</h2>');
+	$('.article').append(`<h3>${weights[$(this).attr('data-content')]}</h3>`);
+	$('.article').append('<hr>');
+	$('.article').append('<h2>Cook Time</h2>');
+	$('.article').append(`<h3>${coockTimes[$(this).attr('data-content')]} /minutes</h3>`);
+	$('.article').append('<hr>');
+	$('.article').append('<h2>Instructions</h2>');
+	$('.article').append(`<a target="_blank" href="${instructions[$(this).attr('data-content')]}"><h3>Click here to see instructions</h3>`);
+
+});
+
+$(".addRecipe").on("click", function (e) {
+	$(".secondpage").hide();
+	$(".recipeList").empty();
+	$(".recipe-list").empty();
+	e.preventDefault();
+
+	userInput = $("#targetRecepi").val().trim().toLowerCase();
+
+	var searchURL = queryURLbase + userInput;
+	doAjax(searchURL);
+	$("#targetRecepi").val("");
+});
